9b14522b47538dfe289da9ac3fbfdb7d9df10247
ShubhamWadhwa-12 <wadhwashubam432@gmail.com>
2026-02-12 19:28:24 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer } from 'react-toastify';
 
 const App = () => {
 
-  const url = "http://localhost:4000"
+  const url = "https://food-delivery-backend-3mcj.onrender.com/"
 
   return (
     <div>
